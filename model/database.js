8c4067dbdf890549392a1a6419c229ea8f48f6b4
Zarina Urazgaleyeva <urazgaleyeva.zarina@gmail.com>
2023-05-22 17:14:55 +0200

    Added sql query into the boilerplate to create three initial tables, and populate the symptoms table

--- model/database.js
+++ model/database.js
@@ -10,19 +10,67 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "facebook",
+  database: DB_NAME || "cycle",
   multipleStatements: true
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 
   let sql =
-    "DROP TABLE if exists students; CREATE TABLE students(id INT NOT NULL AUTO_INCREMENT, firstname VARCHAR(40) not null, lastname VARCHAR(40) not null, PRIMARY KEY (id));";
-  con.query(sql, function(err, result) {
+    `DROP TABLE if exists days_symptoms;
+    DROP TABLE if exists symptoms;
+    DROP TABLE if exists days;
+
+  CREATE TABLE symptoms (
+	id INT NOT NULL AUTO_INCREMENT,
+	symptom_name VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE days_symptoms (
+	day_of_cycle INT NOT NULL,
+	symptom_id INT NOT NULL
+);
+
+CREATE TABLE days (
+	id INT NOT NULL AUTO_INCREMENT,
+	day_of_cycle INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE days_symptoms ADD CONSTRAINT days_symptoms_fk0 FOREIGN KEY (day_of_cycle) REFERENCES days(id);
+
+ALTER TABLE days_symptoms ADD CONSTRAINT days_symptoms_fk1 FOREIGN KEY (symptom_id) REFERENCES symptoms(id);
+
+INSERT INTO symptoms (symptom_name) VALUES
+  ('Moderate to heavy menstrual bleeding'),
+  ('Lighter menstrual bleeding or spotting'),
+  ('Abdominal cramps (dysmenorrhea)'),
+  ('Increased fatigue or tiredness'),
+  ('Headaches or migraines'),
+  ('Backaches or lower back pain'),
+  ('Breast tenderness or sensitivity'),
+  ('Mood swings, irritability, or emotional changes'),
+  ('Decreased menstrual bleeding or spotting'),
+  ('Lightened mood and increased energy levels'),
+  ('Improved concentration and cognitive function'),
+  ('Increased sexual desire and libido'),
+  ('Breast tenderness or mild sensitivity'),
+  ('Mild bloating or water retention'),
+  ('Fewer PMS symptoms'),
+  ('Change in vaginal discharge (thin, clear, and slippery, resembling egg whites)'),
+  ('Mild pelvic pain or discomfort on one side (mittelschmerz) due to the release of the egg'),
+  ('Bloating or water retention'),
+  ('Abdominal cramps or mild pelvic pain'),
+  ('Food cravings or changes in appetite'),
+  ('Acne breakouts or changes in skin'),
+  ('Nausea or digestive issues (e.g., bloating, constipation, or diarrhea)');`;
+
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `students` was successful!");
+    console.log("Tables creation was successful!");
 
     console.log("Closing...");
   });
