43b4280c85b77c27e318fff0b5ec5d13f1599039
Zarina Urazgaleyeva <urazgaleyeva.zarina@gmail.com>
2023-05-23 12:14:31 +0200

    Added values into table days_symptoms, added routes on the backend for getting ALL symptoms and symptoms for a SPECIFIC day

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Students from "./pages/Students";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-<h1>Hello</h1>
+      <h1>Hello</h1>
+      <input type="date" />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Go back to home page</Link>
+          </li>
+          <li>
+            <Link to="/students">Go back to students</Link>
+          </li>
+        </ul>
+      </nav>
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<Students />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//GET ALL symptoms
+router.get('/', async function (req, res, next) {
+  try {
+    const results = await db(`SELECT * FROM symptoms`);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
 });
+
+//GET symptoms per specific day_of_cycle
+router.get('/:day', async function (req, res, next) {
+
+  try {
+    const results = await db(`SELECT * FROM days_symptoms WHERE day_of_cycle = ${req.params.day};`);
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+});
+
+
+
 
 module.exports = router;
