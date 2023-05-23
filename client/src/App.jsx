48eefae1c34d113140b285a341f98491063b861f
Zarina Urazgaleyeva <urazgaleyeva.zarina@gmail.com>
2023-05-23 16:54:24 +0200

    Created Home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,67 +5,16 @@ import Home from "./pages/Home";
 import Students from "./pages/Students";
 
 function App() {
-  const [cycleStartDate, setCycleStartDate] = useState("");
-  const [currentDate, setCurrentDate] = useState(null);
-  const [differenceInDays, setDifferenceInDays] = useState(null);
-
-  useEffect(() => {
-    const getCurrentDate = () => {
-      const today = new Date();
-      setCurrentDate(today.toLocaleString());
-    };
-
-    getCurrentDate();
-  }, []);
-
-  const calculateDifference = (e) => {
-    e.preventDefault();
-    const differenceInMilliseconds =
-      new Date() - new Date(cycleStartDate);
-    const differenceInDays = Math.floor(
-      differenceInMilliseconds / (1000 * 60 * 60 * 24)
-    );
-    setDifferenceInDays(differenceInDays);
-  };
-
-  const handleChange = (event) => {
-    setCycleStartDate(event.target.value);
-  };
 
   return (
     <>
-      <h1>Hello</h1>
-      <h3>Current Date: {currentDate}</h3>
-      <form onSubmit={calculateDifference}>
-        <label>What is the date of the start of your current cycle? </label>
-        <input type="date" value={cycleStartDate} onChange={handleChange} />
-        <button>Submit</button>
-      </form>
-
-      <h3>Your current cycle started on {cycleStartDate}</h3>
-      {differenceInDays !== null && (
-        <h3>You are currently on day {differenceInDays} of your cycle</h3>
-      )}
-    </>
-  );
-}
-
-export default App;
-
-
-
-
-
-
-
-{
-  /* <nav>
+      <nav>
         <ul>
           <li>
-            <Link to="/">Go back to home page</Link>
+            <Link to="/">Home page</Link>
           </li>
           <li>
-            <Link to="/students">Go back to students</Link>
+            <Link to="/students">Symptoms</Link>
           </li>
         </ul>
       </nav>
@@ -73,5 +22,10 @@ export default App;
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/students" element={<Students />} />
-      </Routes> */
+      </Routes>
+
+    </>
+  );
 }
+
+export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
 export default function Home() {
+  const [cycleStartDate, setCycleStartDate] = useState("");
+  const [currentDate, setCurrentDate] = useState(null);
+  const [differenceInDays, setDifferenceInDays] = useState(null);
+
+  useEffect(() => {
+    const getCurrentDate = () => {
+      const today = new Date();
+      setCurrentDate(today.toLocaleString().substring(0, 10));
+    };
+
+    getCurrentDate();
+  }, []);
+
+  const calculateDifference = (e) => {
+    e.preventDefault();
+    const differenceInMilliseconds = new Date() - new Date(cycleStartDate);
+    const differenceInDays = Math.floor(
+      differenceInMilliseconds / (1000 * 60 * 60 * 24)
+    );
+    setDifferenceInDays(differenceInDays);
+  };
+
+  const handleChange = (event) => {
+    const inputDate = event.target.value;
+    console.log(inputDate);
+    setCycleStartDate(inputDate);
+  };
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <h1>Hello</h1>
+      <h3>Current Date: {currentDate}</h3>
+      <form onSubmit={calculateDifference}>
+        <label>What is the date of the start of your current cycle? </label>
+        <input type="date" value={cycleStartDate} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
+
+      <h3>Your current cycle started on {cycleStartDate}</h3>
+      {differenceInDays !== null && (
+        <h3>You are currently on day {differenceInDays} of your cycle</h3>
+      )}
+    </>
+  );
 }
