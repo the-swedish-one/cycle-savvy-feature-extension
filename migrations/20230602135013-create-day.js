411b9d347aec72fd9a77dac4c489aebde00babf5
the-swedish-one <a.damgaard@oxon.org>
2023-06-02 15:51:11 +0200

    Initial commit

---
+++ migrations/20230602135013-create-day.js
@@ -0,0 +1,28 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Days', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      dayOfCycle: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Days');
+  }
+};
---
+++ models/symptom.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Symptom extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Symptom.init({
+    symptomName: DataTypes.STRING,
+    selfCareTips: DataTypes.TEXT,
+    partnerSupportTips: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Symptom',
+  });
+  return Symptom;
+};
