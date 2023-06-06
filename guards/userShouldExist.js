0a4d1c43cda54b3ff814312ad57f7de256328d14
the-swedish-one <a.damgaard@oxon.org>
2023-06-06 17:35:58 +0200

    Uncomment for use

--- guards/userShouldExist.js
+++ guards/userShouldExist.js
@@ -1,20 +1,20 @@
-// const models = require("../models");
+const models = require("../models");
 
-// async function userShouldExist(req, res, next) {
-//   const { id } = req.params;
-//   try {
-//     const user = await models.User.findOne({
-//       where: {
-//         id,
-//       },
-//     });
-//     console.log(user);
-//     if (user.length > 0) {
-//       next();
-//     } else response.status(404).send({ message: "User was not found" });
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// }
+async function userShouldExist(req, res, next) {
+  const { id } = req.params;
+  try {
+    const user = await models.User.findOne({
+      where: {
+        id,
+      },
+    });
+    console.log(user);
+    if (user.id) {
+      next();
+    } else response.status(404).send({ message: "User was not found" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
 
-// module.exports = userShouldExist;
+module.exports = userShouldExist;
