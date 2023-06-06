989e0cdc1609dc8a806b070717181f62dedf6228
the-swedish-one <a.damgaard@oxon.org>
2023-06-06 15:28:57 +0200

    Add endpoint to get user by id

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const models = require("../models");
+// const userShouldExist = require("..guards/userShouldExist");
 
 //GET ALL users
 router.get("/", async function (req, res) {
@@ -9,6 +10,23 @@ router.get("/", async function (req, res) {
     res.send(users);
   } catch (error) {
     res.status(500).send(error);
+  }
+});
+
+// GET user by id
+router.get("/:id", async function (req, res, next) {
+  const { id } = req.params;
+
+  try {
+    const user = await models.User.findOne({
+      where: {
+        id,
+      },
+    });
+
+    res.send(user);
+  } catch (err) {
+    res.status(500).send({ message: err });
   }
 });
 
