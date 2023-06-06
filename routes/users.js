a96208538fe3720fe269b9aec528f99ce3bfee6b
the-swedish-one <a.damgaard@oxon.org>
2023-06-06 15:09:18 +0200

    Replaced prev routes with sequelized user routes

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,14 @@
 var express = require("express");
 var router = express.Router();
-const db = require("../modelOLD/helper");
+const models = require("../models");
 
-//GET ALL symptoms
-router.get("/", async function (req, res, next) {
+//GET ALL users
+router.get("/", async function (req, res) {
   try {
-    const results = await db(`SELECT * FROM symptoms`);
-    res.send(results.data);
-  } catch (err) {
-    res.status(500).send({ message: err });
-  }
-});
-
-//GET all columns (symptom_name, self_care_tips, partner_support_tips) from symptoms table per specific day_of_cycle
-router.get("/days/:day/symptoms", async function (req, res, next) {
-  try {
-    const results = await db(
-      `SELECT * FROM symptoms AS s LEFT JOIN days_symptoms as ds ON s.id = ds.symptom_id WHERE day_of_cycle = ${req.params.day};`
-    );
-
-    res.send(results.data);
-  } catch (err) {
-    res.status(500).send({ message: err });
+    const users = await models.User.findAll();
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
