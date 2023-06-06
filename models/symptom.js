cd03d1db37a642d6bf96b43e2f9d41d6d1948b12
the-swedish-one <a.damgaard@oxon.org>
2023-06-06 15:04:38 +0200

    Add M2M association to day

--- models/symptom.js
+++ models/symptom.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Symptom extends Model {
     /**
@@ -11,15 +10,19 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Symptom.belongsToMany(models.Day, { through: "DaysSymptoms" });
     }
   }
-  Symptom.init({
-    symptomName: DataTypes.STRING,
-    selfCareTips: DataTypes.TEXT,
-    partnerSupportTips: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Symptom',
-  });
+  Symptom.init(
+    {
+      symptomName: DataTypes.STRING,
+      selfCareTips: DataTypes.TEXT,
+      partnerSupportTips: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Symptom",
+    }
+  );
   return Symptom;
 };
