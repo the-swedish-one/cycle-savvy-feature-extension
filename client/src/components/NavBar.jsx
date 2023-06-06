9cb8d4961020fbdeffc85d0e6f2306d620760059
the-swedish-one <a.damgaard@oxon.org>
2023-06-06 14:59:54 +0200

    Initial commit

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+// import { useContext } from "react";
+// import { Link } from "react-router-dom";
+// import AuthContext from "../contexts/AuthContext";
+
+// export default function NavBar() {
+//   return (
+//     <div>
+//       <nav>
+//         <ul>
+//           <li>
+//             <img src="../CycleSavvyLogoTransparent2.png" alt="" width="200px" />
+//           </li>
+//           <li>{ auth.user ?
+//           <button onClick={auth.logout}>Logout</button>
+//           :
+//           <Link to="">Login<Link />
+//           }
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// }
