b4a52e7c8e5275840535880ed2a4d1bd5e6c564b
the-swedish-one <a.damgaard@oxon.org>
2023-06-07 13:28:34 +0200

    Comment out code until admin user has been created

--- routes/users.js
+++ routes/users.js
@@ -4,30 +4,30 @@ const models = require("../models");
 // const userShouldExist = require("..guards/userShouldExist");
 
 //GET ALL users
-router.get("/", async function (req, res) {
-  try {
-    const users = await models.User.findAll();
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+// router.get("/", async function (req, res) {
+//   try {
+//     const users = await models.User.findAll();
+//     res.send(users);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
 
-// GET user by id
-router.get("/:id", async function (req, res, next) {
-  const { id } = req.params;
+// // GET user by id
+// router.get("/:id", async function (req, res, next) {
+//   const { id } = req.params;
 
-  try {
-    const user = await models.User.findOne({
-      where: {
-        id,
-      },
-    });
+//   try {
+//     const user = await models.User.findOne({
+//       where: {
+//         id,
+//       },
+//     });
 
-    res.send(user);
-  } catch (err) {
-    res.status(500).send({ message: err });
-  }
-});
+//     res.send(user);
+//   } catch (err) {
+//     res.status(500).send({ message: err });
+//   }
+// });
 
 module.exports = router;
