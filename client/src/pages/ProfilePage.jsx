df9299cc3cc602617c0f69b9cc13d6d2e83ad5f3
the-swedish-one <a.damgaard@oxon.org>
2023-06-07 12:05:51 +0200

    Show auth-required data

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    requestData();
+  }, []);
+
+  const requestData = async () => {
+    try {
+      const { data } = await axios("/api/auth/profile", {
+        headers: {
+          authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      setData(data);
+      console.log(data.message);
+    } catch (error) {
+      console.log(error);
+      setData(error.message);
+    }
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h2>My Profile</h2>
+      <h5 className="mt-3">ProfilePage: {data?.message} </h5>
+    </div>
+  );
 }
